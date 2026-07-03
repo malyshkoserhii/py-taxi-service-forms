87afba8866098ded7838f839d31afe513f93bc85
malyshkoserhii <malyshkosergiy@gmail.com>
2026-07-03 11:59:49 +0300

    fix: Fixed slash

--- taxi/urls.py
+++ taxi/urls.py
@@ -38,7 +38,7 @@ urlpatterns = [
         name="manufacturer-update"
     ),
     path(
-        "manufacturers/<int:pk>/delete",
+        "manufacturers/<int:pk>/delete/",
         ManufacturerDeleteView.as_view(),
         name="manufacturer-delete"
     ),
